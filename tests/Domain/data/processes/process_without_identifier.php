cdeea70e873a2fb9c07221fbd0428fac113c8c44
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-01-08 16:31:44 +0100

    Process without a identifier (eg. a deposit) wasn't handled correctly

---
+++ tests/Domain/data/processes/process_without_identifier.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+return [
+    'id' => 46069000,
+    'user' => 'johndoe/died',
+    'customer' => 'johndoe',
+    'status' => 'COMPLETED',
+    'createdDate' => '2021-08-11T06:17:15Z',
+    'updatedDate' => '2021-08-25T06:17:15Z',
+    'startedDate' => '2021-08-11T06:17:15Z',
+    'action' => 'deposit',
+    'type' => 'billing',
+    'command' => [
+        'customer' => 'johndoe',
+        'amount' => 1337,
+        'currency' => 'EUR',
+        'method' => 'PAYPAL',
+        'reference' => 'Test',
+        'userIp' => '127.0.0.1',
+        'paymentCurrency' => 'EUR',
+    ],
+];
