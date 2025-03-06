1741b60c9791febb33c46f722c9ffb66738e579d
Hylke <1644844+zbrag@users.noreply.github.com>
2025-03-06 10:49:57 +0100

    SSL-74 Some Certificate downloadformats were missing from the implementation (#23)

--- src/Domain/Enum/DownloadFormatEnum.php
+++ src/Domain/Enum/DownloadFormatEnum.php
@@ -4,12 +4,18 @@ namespace RealtimeRegister\Domain\Enum;
 
 class DownloadFormatEnum extends AbstractEnum
 {
+    const CSR_FORMAT = 'CSR';
     const CRT_FORMAT = 'CRT';
+    const ZIP_FORMAT = 'ZIP';
+    const PKCS7_FORMAT = 'PKCS7';
     const CA_FORMAT = 'CA';
     const CA_BUNDLE_FORMAT = 'CA_BUNDLE';
 
     protected static array $values = [
+        DownloadFormatEnum::CSR_FORMAT,
         DownloadFormatEnum::CRT_FORMAT,
+        DownloadFormatEnum::ZIP_FORMAT,
+        DownloadFormatEnum::PKCS7_FORMAT,
         DownloadFormatEnum::CA_FORMAT,
         DownloadFormatEnum::CA_BUNDLE_FORMAT,
     ];
