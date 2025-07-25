18bd7a71f671b06be2a4cb374a9961e564d1f32b
Hylke <1644844+zbrag@users.noreply.github.com>
2025-07-25 10:06:33 +0200

    RT-16702 We are missing a lot of languages in the locales (#35)

--- src/Domain/Enum/LocaleEnum.php
+++ src/Domain/Enum/LocaleEnum.php
@@ -6,10 +6,36 @@ class LocaleEnum extends AbstractEnum
 {
     const LOCALE_EN_US = 'en-US';
     const LOCALE_NL_NL = 'nl-NL';
+    const LOCALE_KO_KR = 'ko-KR';
+    const LOCALE_TR_TR = 'tr-TR';
+    const LOCALE_IT_IT = 'it-IT';
+    const LOCALE_FR_FR = 'fr-FR';
+    const LOCALE_UK_UA = 'uk-UA';
+    const LOCALE_DA_DK = 'da-DK';
+    const LOCALE_ES_ES = 'es-ES';
+    const LOCALE_PT_PT = 'pt-PT';
+    const LOCALE_DE_DE = 'de-DE';
+    const LOCALE_FI_FI = 'fi-FI';
+    const LOCALE_ET_ET = 'et-ET';
+    const LOCALE_RU_RU = 'ru-RU';
+    const LOCALE_SV_SE = 'sv-SE';
 
     protected static array $values = [
         LocaleEnum::LOCALE_EN_US,
         LocaleEnum::LOCALE_NL_NL,
+        LocaleEnum::LOCALE_KO_KR,
+        LocaleEnum::LOCALE_TR_TR,
+        LocaleEnum::LOCALE_IT_IT,
+        LocaleEnum::LOCALE_FR_FR,
+        LocaleEnum::LOCALE_UK_UA,
+        LocaleEnum::LOCALE_DA_DK,
+        LocaleEnum::LOCALE_ES_ES,
+        LocaleEnum::LOCALE_PT_PT,
+        LocaleEnum::LOCALE_DE_DE,
+        LocaleEnum::LOCALE_FI_FI,
+        LocaleEnum::LOCALE_ET_ET,
+        LocaleEnum::LOCALE_RU_RU,
+        LocaleEnum::LOCALE_SV_SE,
     ];
 
     /** @param string $value */
