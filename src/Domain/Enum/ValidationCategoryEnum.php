f21942d7df6ce57ccfcbb82c994fe12404bdefbe
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2025-09-18 16:04:44 +0200

    RT-16954: update validation categories

--- src/Domain/Enum/ValidationCategoryEnum.php
+++ src/Domain/Enum/ValidationCategoryEnum.php
@@ -5,19 +5,15 @@ namespace RealtimeRegister\Domain\Enum;
 class ValidationCategoryEnum extends AbstractEnum
 {
     const VALIDATION_CATEGORY_GENERAL = 'General';
-    const VALIDATION_CATEGORY_ISI_NU = 'IisNu';
-    const VALIDATION_CATEGORY_ISI_SE = 'IisSe';
-    const VALIDATION_CATEGORY_ESNIC = 'Esnic';
+    const VALIDATION_CATEGORY_IIS_NU = 'IisNu';
+    const VALIDATION_CATEGORY_IIS_SE = 'IisSe';
     const VALIDATION_CATEGORY_NOMINET = 'Nominet';
-    const VALIDATION_CATEGORY_DK_HOSTMASTER = 'DkHostmaster';
 
     protected static array $values = [
         ValidationCategoryEnum::VALIDATION_CATEGORY_GENERAL,
-        ValidationCategoryEnum::VALIDATION_CATEGORY_ISI_NU,
-        ValidationCategoryEnum::VALIDATION_CATEGORY_ISI_SE,
-        ValidationCategoryEnum::VALIDATION_CATEGORY_ESNIC,
+        ValidationCategoryEnum::VALIDATION_CATEGORY_IIS_NU,
+        ValidationCategoryEnum::VALIDATION_CATEGORY_IIS_SE,
         ValidationCategoryEnum::VALIDATION_CATEGORY_NOMINET,
-        ValidationCategoryEnum::VALIDATION_CATEGORY_DK_HOSTMASTER,
     ];
 
     /** @param string $value */
