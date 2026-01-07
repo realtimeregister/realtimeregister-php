f9b384714b29a2aa55048c1e2982ed67688c0c3c
Hylke Witjens <hylke.witjens@realtimeregister.com>
2026-01-07 16:44:26 +0100

    RT-17625 Without price changes or promotions, the request would fail

--- tests/Clients/Customers/CustomersApiListPriceChangesTest.php
+++ tests/Clients/Customers/CustomersApiListPriceChangesTest.php
@@ -30,4 +30,22 @@ class CustomersApiListPriceChangesTest extends TestCase
         $response = $sdk->customers->priceChangesList('johndoe');
         $this->assertInstanceOf(PriceChangesCollection::class, $response);
     }
+
+    public function test_empty_price_changes_list(): void
+    {
+        $sdk = MockedClientFactory::makeSdk(
+            200,
+            json_encode([
+                'pagination' => [
+                    'total'  => 0,
+                    'offset' => 0,
+                    'limit'  => 10,
+                ],
+            ]),
+            MockedClientFactory::assertRoute('GET', 'v2/customers/johndoe/pricelist', $this)
+        );
+
+        $response = $sdk->customers->priceChangesList('johndoe');
+        $this->assertInstanceOf(PriceChangesCollection::class, $response);
+    }
 }
--- tests/Clients/Customers/CustomersApiListPromosTest.php
+++ tests/Clients/Customers/CustomersApiListPromosTest.php
@@ -30,4 +30,22 @@ class CustomersApiListPromosTest extends TestCase
         $response = $sdk->customers->promoList('johndoe');
         $this->assertInstanceOf(PromoCollection::class, $response);
     }
+
+    public function test_empty_promo_list(): void
+    {
+        $sdk = MockedClientFactory::makeSdk(
+            200,
+            json_encode([
+                'pagination' => [
+                    'total'  => 0,
+                    'offset' => 0,
+                    'limit'  => 10,
+                ],
+            ]),
+            MockedClientFactory::assertRoute('GET', 'v2/customers/johndoe/pricelist', $this)
+        );
+
+        $response = $sdk->customers->promoList('johndoe');
+        $this->assertInstanceOf(PromoCollection::class, $response);
+    }
 }
